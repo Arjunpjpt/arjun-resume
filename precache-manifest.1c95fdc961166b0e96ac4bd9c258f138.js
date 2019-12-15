340e0beb6a4075ddf2489120c1eebc64c5c04c69
Arjunpjpt <arjunpjpt@gmail.com>
2019-12-15 00:56:21 -0800

    Updates

---
+++ precache-manifest.1c95fdc961166b0e96ac4bd9c258f138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea3d8530bd5d3e092980e8ca84b702e",
+    "url": "/arjun-resume/index.html"
+  },
+  {
+    "revision": "3ce210157847f708d49d",
+    "url": "/arjun-resume/static/css/main.d5aa1b6a.chunk.css"
+  },
+  {
+    "revision": "aec5f3c8bae81c8d5d63",
+    "url": "/arjun-resume/static/js/2.1bad3f33.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/arjun-resume/static/js/2.1bad3f33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ce210157847f708d49d",
+    "url": "/arjun-resume/static/js/main.3e532495.chunk.js"
+  },
+  {
+    "revision": "74e6b773068ae7e62c07",
+    "url": "/arjun-resume/static/js/runtime-main.802d2aff.js"
+  }
+]);
